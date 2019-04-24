65313e99b3c204bad87e0455337e12a70ac2c245
yifeng <9282207@qq.com>
2019-04-24 10:07:28 +0800

    分布式服务监控

---
+++ yfshop-admin/src/main/java/com/yifeng/yfshop/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.yifeng.yfshop.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
