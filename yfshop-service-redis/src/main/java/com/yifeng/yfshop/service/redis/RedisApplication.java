1f3efd045dc981ce7eaa14488a245d50aaa1fb39
yifeng <9282207@qq.com>
2019-04-24 10:23:58 +0800

    缓存服务提供者

---
+++ yfshop-service-redis/src/main/java/com/yifeng/yfshop/service/redis/RedisApplication.java
@@ -0,0 +1,13 @@
+package com.yifeng.yfshop.service.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
---
+++ yfshop-service-redis/src/main/java/com/yifeng/yfshop/service/redis/service/RedisService.java
@@ -0,0 +1,13 @@
+package com.yifeng.yfshop.service.redis.service;
+
+public interface RedisService {
+    /***
+     *
+     * @param key
+     * @param value
+     * @param seconds  超时时间
+     */
+    public void put(String key, Object value, long seconds);
+
+    public Object get(String key);
+}
